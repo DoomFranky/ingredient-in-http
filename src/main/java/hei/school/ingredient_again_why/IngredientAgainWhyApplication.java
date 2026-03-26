7d64de2593cf9a3b5e4c56db82c373fa77925f0d
Randy <raoulrandy3@gmail.com>
2026-03-26 16:36:56 +0300

    first commit

---
+++ src/main/java/hei/school/ingredient_again_why/IngredientAgainWhyApplication.java
@@ -0,0 +1,13 @@
+package hei.school.ingredient_again_why;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientAgainWhyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientAgainWhyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/school/ingredient_again_why/IngredientAgainWhyApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.ingredient_again_why;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientAgainWhyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
