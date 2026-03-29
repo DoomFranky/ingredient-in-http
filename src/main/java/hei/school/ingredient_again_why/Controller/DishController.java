5c7ed26fb5e2173025160c1c5f47816bf68512c5
Randy <raoulrandy3@gmail.com>
2026-03-29 13:51:03 +0300

    feat: all controller

---
+++ src/main/java/hei/school/ingredient_again_why/Controller/DishController.java
@@ -0,0 +1,16 @@
+package hei.school.ingredient_again_why.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DishController {
+    
+    public ResponseEntity<?> getDishes () {
+        throw new RuntimeException("mehtode not implemented");
+    }
+
+    public ResponseEntity<?> getDishIngredientById () {
+        throw new RuntimeException("mehtode not implemented");
+    }
+}
---
+++ src/main/java/hei/school/ingredient_again_why/Controller/IngredientController.java
@@ -0,0 +1,20 @@
+package hei.school.ingredient_again_why.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IngredientController {
+    
+    public ResponseEntity<?> getIngredients(){
+        throw new RuntimeException("methode not implemented");
+    }
+
+    public ResponseEntity<?> getIngredientById(){
+        throw new RuntimeException("methode not implemented");
+    }
+
+    public ResponseEntity<?> getIngredientStockById(){
+        throw new RuntimeException("methode not implemented");
+    }
+}
