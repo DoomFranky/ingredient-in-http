3b02cb1c8f7e55fe0438767580e094e94c52815d
Randy <raoulrandy3@gmail.com>
2026-03-31 07:12:31 +0300

    feat: the ingredient controller

--- src/main/java/hei/school/ingredient_again_why/Controller/IngredientController.java
+++ src/main/java/hei/school/ingredient_again_why/Controller/IngredientController.java
@@ -1,13 +1,29 @@
 package hei.school.ingredient_again_why.Controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import hei.school.ingredient_again_why.Entity.Ingredients;
+import hei.school.ingredient_again_why.Service.IngredientService;
 
 @RestController
 public class IngredientController {
-    
+    IngredientService ingredientService = new IngredientService();
+
+    @GetMapping("/ingredients")
     public ResponseEntity<?> getIngredients(){
-        throw new RuntimeException("methode not implemented");
+        List<Ingredients> listOfIngredient = ingredientService.getIngredients();
+        try {
+            return ResponseEntity
+                .status(200)
+                .body(listOfIngredient);
+        } catch (Exception e) {
+            return ResponseEntity.status(500)
+                .body(e.getMessage());
+        } 
     }
 
     public ResponseEntity<?> getIngredientById(){
